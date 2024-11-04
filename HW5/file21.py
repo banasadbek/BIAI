366954ac48a53f949c7985b7564fef7f03257a9b
Asadbek <asadbekbekturdiyev06@gmail.com>
2024-11-04 16:32:47 +0500

    update

--- HW5/file21.py
+++ HW5/file21.py
@@ -1,8 +1,7 @@
-num_letters_per_line = 5 
+num = 5 
 file = open('alphabet.txt', 'w')
 for i in range(ord('A'), ord('Z') + 1):
     file.write(chr(i))
-    if (i - ord('A') + 1) % num_letters_per_line == 0:
+    if (i - ord('A') + 1) % num == 0:
         file.write('\n')
 file.close()
-print("Alphabet written to alphabet.txt with", num_letters_per_line, "letters per line.")
